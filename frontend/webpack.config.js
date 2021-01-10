7ec30853687a3a06e0c7f153b73d6e5da213b462
hide <razyetts@hotmail.co.jp>
2021-01-10 17:07:54 +0900

    manifet作成。ヘルパーも作成

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -6,4 +7,9 @@ module.exports = {
     filename: 'main-[contenthash].js',
     path: path.resolve(__dirname, '../public/packs'),
   },
+  plugins: [
+    new WebpackManifestPlugin({
+      publicPath: '/packs/'
+    })
+  ]
 };
