20cea6578558c5921cc6469e84b2b27d9d27d0f1
hide <razyetts@hotmail.co.jp>
2021-01-11 08:45:53 +0900

    ログインしてコールバックまで

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: 'top#index'
   get '/home', to: 'home#index'
   get '/health', to: 'health#index'
+  get '/auth/twitter/callback', to: 'sessions#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
