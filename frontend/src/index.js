2de835d9f06e3175321d416ed39109372ad396d6
hide <razyetts@hotmail.co.jp>
2021-01-10 18:40:41 +0900

    stimulus!

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,10 @@
 import "tailwindcss/tailwind.css"
 
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
+
 console.count('hello, wrold')
