c61b5aab25921f2ee19d95438654f06353a16163
hide <razyetts@hotmail.co.jp>
2021-01-11 09:49:02 +0900

    でばっぐ

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  p "ここちゃんと入ってるか", ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET']
   provider :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET']
 end
