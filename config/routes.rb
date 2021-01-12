e9fed39bef7736777d98ce4f23130e7d1f59789a
hide <razyetts@hotmail.co.jp>
2021-01-12 22:20:19 +0900

    homeをuserに変えようかな

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'top#index'
-  get '/home', to: 'home#index'
   get '/health', to: 'health#index'
   get '/auth/twitter/callback', to: 'sessions#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/:screen_name', to: 'users#show'
 end
