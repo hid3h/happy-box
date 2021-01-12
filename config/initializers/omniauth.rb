eed4431faada284c28f7c9d15f6f9eae98001ff2
hide <razyetts@hotmail.co.jp>
2021-01-12 20:54:13 +0900

    不要

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  p "ここちゃんと入ってるか", Rails.application.credentials.twitter[:api_key], Rails.application.credentials.twitter[:api_secret]
   provider :twitter, Rails.application.credentials.twitter[:api_key], Rails.application.credentials.twitter[:api_secret]
 end
