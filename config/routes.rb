f6fbe58c4f9361a0cb99d24d8543a31af4b9cd42
hide <razyetts@hotmail.co.jp>
2021-01-14 20:45:42 +0900

    ログインしてたらリダイレクト

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get '/health', to: 'health#index'
   get '/auth/twitter/callback', to: 'sessions#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/:screen_name', to: 'users#show'
+  
+  # see https://railsguides.jp/routing.html#%E5%90%8D%E5%89%8D%E4%BB%98%E3%81%8D%E3%83%AB%E3%83%BC%E3%83%86%E3%82%A3%E3%83%B3%E3%82%B0
+  get '/:screen_name', to: 'users#show' , as: :user
 end
