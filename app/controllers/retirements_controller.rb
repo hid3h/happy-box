40d9a1099c23a2a86931bf68d924265a6ec1df44
hide <razyetts@hotmail.co.jp>
2021-01-14 21:15:33 +0900

    退会コントローラ作成。中身はまだ

---
+++ app/controllers/retirements_controller.rb
@@ -0,0 +1,8 @@
+class RetirementsController < ApplicationController
+  def new
+  end
+
+  def create
+    p "cre"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get '/health', to: 'health#index'
   get '/auth/twitter/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+
+  resource :retirements
   
   # see https://railsguides.jp/routing.html#%E5%90%8D%E5%89%8D%E4%BB%98%E3%81%8D%E3%83%AB%E3%83%BC%E3%83%86%E3%82%A3%E3%83%B3%E3%82%B0
   get '/:screen_name', to: 'users#show' , as: :user
