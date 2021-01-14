b921b15ed5fdc56914fff9198ea4d497fc967f1e
hide <razyetts@hotmail.co.jp>
2021-01-14 21:19:34 +0900

    link_toのpost,deleteとか効くのはrails-ujsのおかげなんだ

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,9 @@ import "tailwindcss/tailwind.css"
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 
+import Rails from 'rails-ujs';
+Rails.start();
+
 const application = Application.start()
 const context = require.context("./controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
