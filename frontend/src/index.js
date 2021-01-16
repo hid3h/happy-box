acb47da53f5db2ab98ee36dc18e32139d4cced60
hide <razyetts@hotmail.co.jp>
2021-01-16 15:56:04 +0900

    aタグにtype buttonがだめだった

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,18 @@
 import "tailwindcss/tailwind.css"
 import './style.css'
 
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
-
 import Rails from 'rails-ujs';
 Rails.start();
 const Turbolinks = require("turbolinks")
 Turbolinks.start()
 
+/** stimulus */
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
 const application = Application.start()
 const context = require.context("./controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
+/** stimulus */
 
-console.count('hello, wrold')
+console.count('hello, wrold2')
