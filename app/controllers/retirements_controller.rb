a5d620036354936933b8221deedb14e11904f670
hide <razyetts@hotmail.co.jp>
2021-01-20 21:14:48 +0900

    設定ページのデザイン

--- app/controllers/retirements_controller.rb
+++ app/controllers/retirements_controller.rb
@@ -3,7 +3,7 @@ class RetirementsController < ApplicationController
   end
 
   def create
-    if current_user.detroy
+    if current_user.destroy
       reset_session
       redirect_to root_path, notice: "退会完了しました"
     end
