dbd2b9493f495f43a23881d3f9a1056bced5cbf5
hide <razyetts@hotmail.co.jp>
2021-01-21 19:31:25 +0900

    トップのデザイン考え

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,14 @@ class UsersController < ApplicationController
   skip_before_action :authenticate
   
   def show
-    user = User.find_by!(screen_name: params[:screen_name])
+    # viewにな雨とか表示かな？
+    @user = User.find_by!(screen_name: params[:screen_name])
 
-    @happiness_cards_count_by_month = HappinessCard.count_by_month(user: user)
-    @happiness_cards_count_by_year  = HappinessCard.count_by_year(user: user)
+    @happiness_cards_count_by_month = HappinessCard.count_by_month(user: @user)
+    @happiness_cards_count_by_year  = HappinessCard.count_by_year(user: @user)
     
-    @is_mypage = current_user == user
+    @is_mypage = current_user == @user
 
-    @page_title = user.name + 'さんのしあわせグラフ'
+    @page_title = @user.name + 'さんのしあわせグラフ'
   end
 end
